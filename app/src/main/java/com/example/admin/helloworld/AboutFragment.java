d7679241b4956981f44e8f1c49a87f64ccab0302
eriollee <eriollee@163.com>
2018-09-11 20:47:54 +0800

    完成about功能

--- app/src/main/java/com/example/admin/helloworld/AboutFragment.java
+++ app/src/main/java/com/example/admin/helloworld/AboutFragment.java
@@ -1,5 +1,9 @@
 package com.example.admin.helloworld;
 
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
 /**
  * Created by admin on 2018-09-10.
  */
@@ -7,7 +11,9 @@ package com.example.admin.helloworld;
 public class AboutFragment extends BaseFragment {
     @Override
     protected void initView() {
-
+        TextView textView = bindViewId(R.id.tv_app_des);
+        textView.setAutoLinkMask(Linkify.ALL);//有链接可点
+        textView.setMovementMethod(LinkMovementMethod.getInstance());//文字可以滚动
     }
 
     @Override
